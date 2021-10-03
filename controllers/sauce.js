a059d4b03316e6e479f94e13433c25a6f2262e58
Brice Leblanc <d2_brice@hotmail.fr>
2021-10-03 20:00:31 +0200

    finalisation

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,6 +1,8 @@
 const Sauce = require('../models/Sauce')
 const fs = require('fs');
 
+
+
 exports.createSauce = (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce);
   sauceObject.likes = 0;
@@ -13,7 +15,7 @@ exports.createSauce = (req, res, next) => {
   });
   console.log(sauce)
   sauce.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .then(() => res.status(201).json({ message: 'Sauce enregistré !'}))
     .catch(error => res.status(400).json({ messsage: error.message }));
 };
 
@@ -24,17 +26,18 @@ exports.modifySauce = (req, res, next) => {
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     } : { ...req.body };
   Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .then(() => res.status(200).json({ message: 'Sauce modifié !'}))
     .catch(error => res.status(400).json({ message: error.message }));
 };
 
 exports.deleteSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then(sauce => {
+      // recupere l'identifiant de l'utilisateur qui a créé la sauce et le comparer a l'identifiant de l'utilisateur dans le body de la sauce
       const filename = sauce.imageUrl.split('/images/')[1];
       fs.unlink(`images/${filename}`, () => {
         Sauce.deleteOne({ _id: req.params.id })
-          .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+          .then(() => res.status(200).json({ message: 'Sauce supprimé !'}))
           .catch(error => res.status(400).json({ message: error.message  }));
       });
     })
@@ -48,43 +51,56 @@ exports.deleteSauce = (req, res, next) => {
   }
 
   exports.getAllSauces = (req, res, next) => {
+    console.log(req.user.userId)
     Sauce.find()
       .then(sauces => res.status(200).json(sauces))
       .catch(error => res.status(400).json({message: error.message  }));
   }
 
-  exports.likeDislikeSauce = (req, res, next) => {
-    const like = req.body.like;
-    const userId = req.body.userId;
-    const sauceId = req.params.id;
   
-    switch (like) {  
-      case 1: Sauce.updateOne({ _id: sauceId }, { $inc: { likes: 1 }, $push: { usersLiked: userId } })
-        .then(() => res.status(200).json({ message: 'Sauce likée !' }))
-        .catch(error => res.status(400).json({ error }));
-        break;
-      case 0: Sauce.findOne({ _id: sauceId })
-        .then(sauce => {
-          const likesOrDislikes = {};
-          const usersLikedOrDisliked = {};
-          if (Sauce.usersLiked.includes(userId)) {
-            likesOrDislikes.likes = -1;
-            usersLikedOrDisliked.usersLiked = userId;
-          } else if (Sauce.usersDisliked.includes(userId)) {
-            likesOrDislikes.dislikes = -1;
-            usersLikedOrDisliked.usersDisliked = userId;
-          }
-          Sauce.updateOne({ _id: sauceId }, { $inc: likesOrDislikes, $pull: usersLikedOrDisliked })
-            .then(() => res.status(200).json({ message: 'Modifé !' }))
-            .catch(error => res.status(400).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
-        
-        break;
-      case -1: Sauce.updateOne({ _id: sauceId }, { $inc: { dislikes: 1 }, $push: { usersDisliked: userId } })
-        .then(() => res.status(200).json({ message: 'Sauce dislikée !' }))
-        .catch(error => res.status(400).json({ error }));
-        break;
-    }
-
+  exports.likeDislikeSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+      .then((sauce) => {
+        switch (req.body.like) {
+          case -1: //au clic sur dislike           
+            // si le userId n'est pas dans le tableau userDisliked, on le push
+            if (sauce.usersDisliked.indexOf(req.body.userId) === -1) {
+              sauce.usersDisliked.push(req.body.userId);
+            }
+            break;
+  
+          case 1: //clic sur like
+            //si le userId n'est pas dans le tableau userLiked, on le push
+            if (sauce.usersLiked.indexOf(req.body.userId) === -1) {
+              sauce.usersLiked.push(req.body.userId);
+            }
+            break;
+          case 0: //annule like ou dislike
+            // si le userId est présent dans la tableau usersDisliked,  on le splice
+            if (sauce.usersDisliked.indexOf(req.body.userId) !== -1) {
+              sauce.usersDisliked.splice(
+                sauce.usersDisliked.indexOf(req.body.userId, 1)
+              );
+            }
+            // si le userId est présent dans la tableau usersLiked, on le splice
+            if (sauce.usersLiked.indexOf(req.body.userId) !== -1) {
+              sauce.usersLiked.splice(
+                sauce.usersLiked.indexOf(req.body.userId, 1)
+              );
+            }
+            break;
+          default:
+            return res.status(400).json({ error: "Valeur de like invalide !" });
+        }
+        //Affichage  du nombre de loke et de dislike
+        sauce.likes = sauce.usersLiked.length;
+        sauce.dislikes = sauce.usersDisliked.length;
+        sauce
+          .save()
+          .then(() =>
+            res.status(200).json({ message: "Compteurs de likes mis à jour !" })
+          )
+          .catch((error) => res.status(400).json({ error }));
+      })
+      .catch((error) => res.status(400).json({ error }));
   };
